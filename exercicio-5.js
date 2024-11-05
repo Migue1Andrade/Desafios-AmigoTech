e36b95f1c63f30a4a4be6eefff05dee9bdd05d68
Migue1Andrade <mfrancadeandrade@gmail.com>
2024-11-05 10:39:26 -0300

    question 5

---
+++ exercicio-5.js
@@ -0,0 +1,20 @@
+const dataFormatada = new Date("2022-06-07T01:01:06.336Z");
+
+const options = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit'
+};
+
+dataFormatada.toLocaleDateString('pt-BR', options);
+
+const data = () => {
+    const ano = dataFormatada.getFullYear();
+    const mes = dataFormatada.getMonth() + 1;
+    const dia = dataFormatada.getDate();
+
+    return `${dia}/${mes}/${ano}`;
+};
+
+console.log(data(dataFormatada));
+
