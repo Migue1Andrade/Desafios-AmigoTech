fb96282f2d56f21f2d10b55fff24e55255be6865
Migue1Andrade <mfrancadeandrade@gmail.com>
2024-11-05 00:21:58 -0300

    first complete

--- exercicio-1.js
+++ exercicio-1.js
@@ -1,27 +1,27 @@
 const arrayNumeros = [0, "1", "1.5", 2, 3, 4, 5, 6, 7, "8", 9];
 
-function criarNovoArray(array) {
-  const novoArray = [];
+const novoArray = arrayNumeros.filter((elemento) => typeof elemento === "number");
+console.log(`array sem as strings ${novoArray}`);
 
-  for (let i = 0; i < 8; i++) {
-    let soma = array[i];
-
-    if (i > 0) {
-      soma += array[i - 1];
-    }
-
-    novoArray.push(soma);
-
-    if (soma % 2 !== 0) {
-      novoArray.splice(i, 0, soma + 0.5);
-      
-      i++;
-    }
-  }
-
-  return novoArray;
+function somaArray(array) {
+	for (let i = 0; i < array.length; i++) {
+		array[i] += array[i + 1];
+	}
+	return;
 }
 
-const array = arrayNumeros.filter((elemento) => typeof elemento === "number");
+function adicionarMeioAosAnterioresImpares(array) {
+	for (let i = 1; i < array.length; i++) {
+		if (array[i] % 2 !== 0) { 
+		array[i - 1] += 0.5; 
+		}
+  }
+  return array;
+}
 
-const resultado = criarNovoArray(array);
+somaArray(novoArray);
+const arraySemNaN = novoArray.filter(element => !isNaN(element));
+console.log(`array somado o primeiro com o segundo ${arraySemNaN}`);
+adicionarMeioAosAnterioresImpares(arraySemNaN);
+console.log(`array adicionando 0.5 ao anterior ${arraySemNaN}`);
+console.log(arraySemNaN)
